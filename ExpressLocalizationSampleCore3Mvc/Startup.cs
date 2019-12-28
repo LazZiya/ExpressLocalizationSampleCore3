326180d1cdcb5e8b4a361cf272b99a648d66b00e
Ziya Mollamahmut <ziyadmm@gmail.com>
2019-12-28 08:29:47 +0300

    update nuget packages to latest versions and add localization comments in startup

--- ExpressLocalizationSampleCore3Mvc/Startup.cs
+++ ExpressLocalizationSampleCore3Mvc/Startup.cs
@@ -56,8 +56,25 @@ namespace ExpressLocalizationSampleCore3Mvc
             services.AddControllersWithViews()
                 .AddExpressLocalization<ExpressLocalizationResource,ViewLocalizationResource>(ops=>
                 {
-                    // uncomment and set to true to use only route culture provider
-                    // ops.UseAllCultureProviders = false;
+                    // When using all the culture providers, the localization process will
+                    // check all available culture providers in order to detect the request culture.
+                    // If the request culture is found it will stop checking and do localization accordingly.
+                    // If the request culture is not found it will check the next provider by order.
+                    // If no culture is detected the default culture will be used.
+
+                    // Checking order for request culture:
+                    // 1) RouteSegmentCultureProvider
+                    //      e.g. http://localhost:1234/tr
+                    // 2) QueryStringCultureProvider
+                    //      e.g. http://localhost:1234/?culture=tr
+                    // 3) CookieCultureProvider
+                    //      Determines the culture information for a request via the value of a cookie.
+                    // 4) AcceptedLanguageHeaderRequestCultureProvider
+                    //      Determines the culture information for a request via the value of the Accept-Language header.
+                    //      See the browsers language settings
+
+                    // Uncomment and set to true to use only route culture provider
+                    ops.UseAllCultureProviders = false;
                     ops.ResourcesPath = "LocalizationResources";
                     ops.RequestLocalizationOptions = o =>
                     {
